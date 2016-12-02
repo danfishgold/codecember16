ef95b0e9f6f9e78a0bfcfd19c100009ef0f73afb
Dan Fishgold <d.fishgold@gmail.com>
2016-12-02 18:31:47 +0200

    Added a random palette demo

--- Day2/Main.elm
+++ Day2/Main.elm
@@ -1,18 +1,73 @@
 module Main exposing (..)
 
-import Html exposing (beginnerProgram)
-import Svg exposing (Svg, svg, rect)
+import Html exposing (program)
+import Html exposing (Html, div, button, text)
+import Html.Events exposing (onClick)
+import Svg exposing (svg, rect)
 import Svg.Attributes exposing (x, y, width, height, fill)
 import Day2.Ryb exposing (ryb)
+import Day2.Random exposing (ryb1, ryb1v1, ryb1v2, ryb1v3, ryb2v2)
+import Random exposing (generate, map)
 import Color exposing (Color)
 
 
-main : Program Never {} msg
+type alias Model =
+    List Color
+
+
+type Msg
+    = Set Model
+    | One
+    | OneVOne
+    | OneVTwo
+    | OneVThree
+    | TwoVTwo
+
+
+t1 a =
+    [ a, a, a, a ]
+
+
+t2 ( a, b ) =
+    [ a, a, b, b ]
+
+
+t3 ( a, b, c ) =
+    [ a, a, b, c ]
+
+
+t4 ( a, b, c, d ) =
+    [ a, b, c, d ]
+
+
+update msg model =
+    case msg of
+        Set newModel ->
+            ( newModel, Cmd.none )
+
+        One ->
+            ( model, generate Set (ryb1 1 0.5 |> map t1) )
+
+        OneVOne ->
+            ( model, generate Set (ryb1v1 1 0.5 |> map t2) )
+
+        OneVTwo ->
+            ( model, generate Set (ryb1v2 1 0.5 45 |> map t3) )
+
+        OneVThree ->
+            ( model, generate Set (ryb1v3 1 0.5 45 |> map t4) )
+
+        TwoVTwo ->
+            ( model, generate Set (ryb2v2 1 0.5 45 |> map t4) )
+
+
+main : Program Never Model Msg
 main =
-    beginnerProgram
-        { model = {}
-        , update = flip always
+    program
+        { init = ( [], Cmd.none )
+        , update = update
         , view = view
+        , subscriptions = always Sub.none
         }
 
 
@@ -31,20 +86,37 @@ colorToString color =
            )
 
 
-view : {} -> Svg msg
-view _ =
+view : Model -> Html Msg
+view colors =
     let
-        n =
-            24
+        d =
+            300
 
-        colors =
-            List.range 0 n
-                |> List.map (\i -> ryb (toFloat (360 // n * i)) 1 0.5)
-                |> List.map colorToString
+        n =
+            List.length colors
 
         square i color =
-            rect [ x <| toString <| 50 * i, y <| "0", width "50", height "50", fill color ] []
+            rect
+                [ x <| toString <| d * toFloat (i % 2)
+                , y <| toString <| d * toFloat (i // 2)
+                , width <| toString d
+                , height <| toString d
+                , fill <| colorToString color
+                ]
+                []
     in
-        colors
-            |> List.indexedMap square
-            |> svg [ width "100%", height "100%" ]
+        div []
+            [ div []
+                [ button [ onClick One ] [ text "1" ]
+                , button [ onClick OneVOne ] [ text "1:1" ]
+                , button [ onClick OneVTwo ] [ text "1:2" ]
+                , button [ onClick OneVThree ] [ text "1:3" ]
+                , button [ onClick TwoVTwo ] [ text "2:2" ]
+                ]
+            , colors
+                |> List.indexedMap square
+                |> svg
+                    [ width <| toString <| d * 2
+                    , height <| toString <| d * toFloat (ceiling (d / 2))
+                    ]
+            ]

042ac5710d57fde3bc2652e50eba6503f0bcdac0
Dan Fishgold <d.fishgold@gmail.com>
2016-12-04 11:08:22 +0200

    Pretty much finished
    I just need to add real events.

--- Day4/Main.elm
+++ Day4/Main.elm
@@ -4,7 +4,7 @@ import Html exposing (program)
 import Html exposing (Html, div, text)
 import Html.Attributes exposing (style)
 import AnimationFrame exposing (diffs)
-import Time exposing (Time)
+import Time exposing (Time, second)
 
 
 type alias Model =
@@ -43,17 +43,60 @@ subscriptions model =
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        _ ->
-            ( model, Cmd.none )
+        Tick dt ->
+            let
+                progress ({ current, duration } as event) =
+                    if current + dt > duration then
+                        { event | current = current + dt - duration, fired = True }
+                    else
+                        { event | current = current + dt }
+            in
+                ( { model | events = model.events |> List.map progress }, Cmd.none )
 
 
 
 --
 
 
-view : Model -> Html Msg
-view model =
-    div [] []
+view : String -> Model -> Html Msg
+view side { events } =
+    let
+        eventDiv title opacity =
+            div
+                [ style
+                    [ ( "width", side )
+                    , ( "height", side )
+                    , ( "flex", "0 1 auto" )
+                    , ( "opacity", toString <| opacity )
+                    , ( "display", "flex" )
+                    , ( "text-align", "center" )
+                    , ( "align-items", "center" )
+                    , ( "justify-content", "center" )
+                    ]
+                ]
+                [ text title ]
+
+        event { title, duration, current, fired } =
+            if not fired then
+                eventDiv title 0
+            else
+                eventDiv title <| decay <| 5 * current / duration
+    in
+        events
+            |> List.map event
+            |> div
+                [ style
+                    [ ( "width", "500px" )
+                    , ( "margin", "0 auto" )
+                    , ( "display", "flex" )
+                    , ( "flex-wrap", "wrap" )
+                    ]
+                ]
+
+
+decay : Time -> Float
+decay f =
+    0.15 + 0.85 * e ^ -f
 
 
 
@@ -62,7 +105,9 @@ view model =
 
 events : List ( String, Float )
 events =
-    [ ( "Heartbeat", 1 )
+    [ ( "Heartbeat", 1 * second )
+    , ( "One Birth", 1 * second / 2.5 )
+    , ( "One Death", 1 * second / 2.3 )
     ]
 
 
@@ -72,5 +117,5 @@ main =
         { init = init events
         , subscriptions = subscriptions
         , update = update
-        , view = view
+        , view = view "100px"
         }

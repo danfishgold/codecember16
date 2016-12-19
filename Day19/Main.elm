76481fee520757376b85c3f0301b743c15b2d949
Dan Fishgold <d.fishgold@gmail.com>
2016-12-19 19:25:52 +0200

    It's almost as if it works

--- Day19/Main.elm
+++ Day19/Main.elm
@@ -4,18 +4,26 @@ import Html exposing (program)
 import Svg exposing (Svg, svg, g, text, text_)
 import Svg.Attributes exposing (width, height)
 import Time exposing (Time, second, every)
-import Day19.Trail exposing (Trail)
+import Day19.Trail as Trail exposing (Trail)
+import Random exposing (generate)
+import Random.Extra exposing (combine)
 
 
 type alias Model =
     { width : Float
     , height : Float
     , trails : List Trail
+    , trailLength : Int
+    , count : Int
+    , fontSize : Float
+    , fontFamily : String
+    , time : Int
     }
 
 
 type Msg
     = Tick Time
+    | SetTrails (List Trail)
     | AddTrail Trail
 
 
@@ -24,6 +32,11 @@ init width height =
     ( { width = width
       , height = height
       , trails = []
+      , trailLength = 10
+      , count = 10
+      , fontSize = 12
+      , fontFamily = "monospace"
+      , time = 0
       }
     , Cmd.none
     )
@@ -35,7 +48,7 @@ init width height =
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-    every second Tick
+    every (0.1 * second) Tick
 
 
 
@@ -45,8 +58,32 @@ subscriptions model =
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        _ ->
-            ( model, Cmd.none )
+        Tick _ ->
+            let
+                time =
+                    model.time + 1
+
+                ( wd, ht ) =
+                    ( model.width / model.fontSize |> floor, model.height / model.fontSize |> floor )
+
+                trails =
+                    model.trails |> List.filter (\{ letters, y } -> y - List.length letters <= ht)
+            in
+                ( { model | time = time }
+                , Cmd.batch
+                    [ if List.length model.trails < model.count then
+                        generate AddTrail (Trail.random wd model.trailLength)
+                      else
+                        Cmd.none
+                    , model.trails |> List.map (Trail.update model.time) |> combine |> generate SetTrails
+                    ]
+                )
+
+        SetTrails trails ->
+            ( { model | trails = trails }, Cmd.none )
+
+        AddTrail trail ->
+            ( { model | trails = trail :: model.trails }, Cmd.none )
 
 
 
@@ -55,7 +92,8 @@ update msg model =
 
 view : Model -> Svg Msg
 view model =
-    []
+    model.trails
+        |> List.map (Trail.view model.fontFamily model.fontSize)
         |> svg [ width <| toString model.width, height <| toString model.height ]
 
 

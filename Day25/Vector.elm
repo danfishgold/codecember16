6f801b5c8f0c27b8f7d85ae865945c01ec1722f9
Dan Fishgold <d.fishgold@gmail.com>
2016-12-25 22:04:52 +0200

    Added Vector functions
    arg fits the canvas axes such that positive x is left and positive y is up.

--- Day25/Vector.elm
+++ Day25/Vector.elm
@@ -1,4 +1,4 @@
-module Day25.Vector exposing (Vector, add, sub, mul)
+module Day25.Vector exposing (..)
 
 
 type alias Vector =
@@ -10,9 +10,19 @@ add ( a1, a2 ) ( b1, b2 ) =
     ( a1 + b1, a2 + b2 )
 
 
+sum : List Vector -> Vector
+sum vecs =
+    List.foldl add ( 0, 0 ) vecs
+
+
 sub : Vector -> Vector -> Vector
 sub ( a1, a2 ) ( b1, b2 ) =
     ( a1 - b1, a2 - b2 )
+
+
+neg : Vector -> Vector
+neg ( x, y ) =
+    ( -x, -y )
 
 
 mul : Float -> Vector -> Vector
@@ -28,3 +38,28 @@ norm2 ( v1, v2 ) =
 dist2 : Vector -> Vector -> Float
 dist2 u v =
     sub u v |> norm2
+
+
+arg : Vector -> Float
+arg ( x, y ) =
+    atan2 -x y
+
+
+normalizeOrZero : Float -> Vector -> Vector
+normalizeOrZero mag ( x, y ) =
+    if ( x, y ) == ( 0, 0 ) then
+        ( 0, 0 )
+    else
+        let
+            scale =
+                mag / (sqrt <| norm2 ( x, y ))
+        in
+            ( x * scale, y * scale )
+
+
+onZero : Vector -> Vector -> Vector
+onZero default vec =
+    if vec == ( 0, 0 ) then
+        default
+    else
+        vec

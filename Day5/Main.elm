a01a7f822d78aef94f787e10ff39d492dbf2410d
Dan Fishgold <d.fishgold@gmail.com>
2017-01-03 22:09:29 +0200

    Added switch between 2 and 3 colors in Automaton

--- Day5/Main.elm
+++ Day5/Main.elm
@@ -2,7 +2,7 @@ module Automaton exposing (..)
 
 import Html exposing (program)
 import Helper exposing (project)
-import Html exposing (Html, div)
+import Html exposing (Html, div, button, text)
 import Html.Attributes exposing (style)
 import Svg exposing (Svg, svg, rect, g)
 import Svg.Attributes exposing (x, y, width, height, fill, stroke, strokeWidth, transform)
@@ -30,6 +30,7 @@ message msg =
 type Msg
     = ShiftRule (List Int)
     | AddRowIfNeeded
+    | SetNumColors Int
 
 
 init : Int -> ( Model, Cmd Msg )
@@ -100,6 +101,9 @@ update msg model =
                     ( { model | levels = levels }, message AddRowIfNeeded )
             else
                 ( model, Cmd.none )
+
+        SetNumColors n ->
+            ( { model | colors = n }, Cmd.none )
 
 
 
@@ -232,7 +236,13 @@ view res model =
             , ( "align-items", "center" )
             ]
         ]
-        [ pyramid (res / 2) model, rules res model ]
+        [ pyramid (res / 2) model
+        , rules res model
+        , div []
+            [ button [ onClick <| SetNumColors 2 ] [ text "2 colors" ]
+            , button [ onClick <| SetNumColors 3 ] [ text "3 colors" ]
+            ]
+        ]
 
 
 

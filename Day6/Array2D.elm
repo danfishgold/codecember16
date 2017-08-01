b88e59d052213f5a7a317477253350ad9e2e95f3
Dan Fishgold <d.fishgold@gmail.com>
2017-08-01 23:06:34 +0300

    Rewrote Day6 without ports
    It's slower, but at least Safari won't crash.

---
+++ Day6/Array2D.elm
@@ -0,0 +1,40 @@
+module Day6.Array2D exposing (Array2D, get, set, empty, elementsInSubArray)
+
+import Array exposing (Array)
+
+
+type alias Array2D a =
+    Array (Array (Maybe a))
+
+
+empty : Int -> Int -> Array2D a
+empty n m =
+    Array.repeat n (Array.repeat m Nothing)
+
+
+get : Int -> Int -> Array2D a -> Maybe a
+get i j arr =
+    Array.get i arr
+        |> Maybe.andThen (Array.get j)
+        |> Maybe.andThen identity
+
+
+set : Int -> Int -> Maybe a -> Array2D a -> Array2D a
+set i j a arr =
+    case Array.get i arr of
+        Nothing ->
+            arr
+
+        Just row ->
+            Array.set i (Array.set j a row) arr
+
+
+elementsInSubArray : Array2D a -> List Int -> List Int -> List a
+elementsInSubArray arr rows cols =
+    case rows of
+        [] ->
+            []
+
+        i :: otherRows ->
+            elementsInSubArray arr otherRows cols
+                ++ List.filterMap (\j -> get i j arr) cols
